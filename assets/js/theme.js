7c20e948ce3308f4a3a2bd60af757fcf7e96c158
AztraGroup <aztragroup@gmail.com>
2025-08-17 16:43:37 -0300

    Apply

--- assets/js/theme.js
+++
@@ -1,5 +0,0 @@
-(function($){
-    $(document).ready(function(){
-        console.log('Aztra theme loaded');
-    });
-})(jQuery);
--- plugins/aztra-builder/admin/js/builder.js
+++
@@ -1,5 +0,0 @@
-(function($){
-    $(document).ready(function(){
-        $('#aztra-builder-root').text('Interface do builder em desenvolvimento...');
-    });
-})(jQuery);
